33d065d74b6bf9594e50bb764819e6f89e992450
Oussi108 <live.houssin@gmail.com>
2023-08-07 16:16:42 +0100

    login done

--- MainApp/urls.py
+++ MainApp/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
 urlpatterns = [
+    path('', views.data_visualization_view, name='data_visualization'),
     path('data_visualization/', views.data_visualization_view, name='data_visualization'),
+    path('home/', views.data_visualization_view, name='data_visualization'),
     path('workers/', views.Workers_managment_view, name='workers_managment'),
-    path('login/', views.Login_infra_view, name='login_infra'),
-    path('Upload/', views.Upload_view, name='Upload'),
+    path('login/', views.Login_infra_view, name='login'),
+    path('Upload/', views.Upload_view, name='upload'),
+    path('logout/', views.logout_user, name='logout'),
 ]
--- MainApp/views.py
+++ MainApp/views.py
@@ -1,16 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+
+
 # Create your views here.
 
-
+@login_required(login_url="login")
 def data_visualization_view(request):
     # render(request, 'MainApp/data_visualization.html');
     return render(request, 'data_visualization.html')
+
+
+@login_required(login_url="login")
 def Workers_managment_view(request):
     
     return render(request,'workers_managment.html')
+
+
+
 def Login_infra_view(request):
-    
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        try :
+            user = User.objects.get(username = username)
+        except:
+            messages.error(request, "User doesnt exist")
+
+        user = authenticate(request,username=username,password=password)
+        if user is not None :
+            login(request,user)
+            url = reverse('data_visualization')
+
+            return redirect(url)
+        else :
+            messages.error(request,"user doesnt exist or password is incorrect")
     return render(request,'login_infra.html')
+
+
+@login_required(login_url="login")
 def Upload_view(request):
+    messages.error(request, "not working yet")
     return render(request,'Upload.html')
+   
+def logout_user(request):
+    logout(request)
+    return redirect("login")
+    
